5f236ee17591afabd6c5bcf9214f1af14e081b26
Max Seipio <mseipio@evotechrd.com>
2019-11-10 20:18:29 -0400

    Modules Modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// ROUTES
 import { APP_ROUTES } from './app.routes';
+
+// MODULES
+import { PagesModule } from './components/pages/pages.module';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { MenuComponent } from './components/shared/menu/menu.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { SettingsComponent } from './components/shared/settings/settings.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
-import { ProgressComponent } from './components/pages/progress/progress.component';
 import { LoginComponent } from './components/login/login.component';
-import { BreadcrumbsComponent } from './components/shared/breadcrumbs/breadcrumbs.component';
 import { PagenotfoundComponent } from './components/pages/pagenotfound/pagenotfound.component';
-import { EmployeesListComponent } from './components/pages/employees/employees-list/employees-list.component';
-import { PagesComponent } from './components/pages/pages.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MenuComponent,
-    FooterComponent,
-    SettingsComponent,
-    DashboardComponent,
-    ProgressComponent,
     LoginComponent,
-    BreadcrumbsComponent,
     PagenotfoundComponent,
-    EmployeesListComponent,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTES,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,11 @@
 
 import { Routes, RouterModule } from '@angular/router';
-import { EmployeesListComponent } from './components/pages/employees/employees-list/employees-list.component';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
-import { PagenotfoundComponent } from './components/pages/pagenotfound/pagenotfound.component';
-import { PagesComponent } from './components/pages/pages.component';
-import { LoginComponent } from './components/login/login.component';
 
+import { LoginComponent } from './components/login/login.component';
+import { PagenotfoundComponent } from './components/pages/pagenotfound/pagenotfound.component';
 
 const appRoutes: Routes = [
-  {
-    path: '', component: PagesComponent, children: [
-      { path: 'employeelist', component: EmployeesListComponent },
-      { path: 'dashboard', component: DashboardComponent },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
+
   { path: 'login', component: LoginComponent },
   { path: '**', component: PagenotfoundComponent },
 
